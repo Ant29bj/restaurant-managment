494f7eb32187b803f772bbae949fc3a2227e6f69
SaulRecio06 <76199088+SaulRecio06@users.noreply.github.com>
2022-12-27 23:33:02 -0700

    Prueba

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,6 @@ import { TipoPagosModule } from './tipo_pagos/tipo_pagos.module';
 import { CategoriaMermaModule } from './categoria_merma/categoria_merma.module';
 import { InsumosModule } from './insumos/insumos.module';
 import { EquivalenciasMedidasModule } from './equivalencias_medidas/equivalencias_medidas.module';
-import { ComandaModule } from './comanda/comanda.module';
-import { ComandaProductosModule } from './comanda_productos/comanda_productos.module';
-import { ComandaServiciosModule } from './comanda_servicios/comanda_servicios.module';
-import { ComedorModule } from './comedor/comedor.module';
-import { DetalleRecetaModule } from './detalle_receta/detalle_receta.module';
-import { DiasHabilesModule } from './dias_habiles/dias_habiles.module';
-import { EstadosModule } from './estados/estados.module';
-import { FuncionalidadesModule } from './funcionalidades/funcionalidades.module';
-import { ReservacionesModule } from './reservaciones/reservaciones.module';
-import { RolDeFuncionalidadesModule } from './rol_de_funcionalidades/rol_de_funcionalidades.module';
-import { SucursalesModule } from './sucursales/sucursales.module';
 
 @Module({
   imports: [
@@ -40,17 +29,6 @@ import { SucursalesModule } from './sucursales/sucursales.module';
     CategoriaMermaModule,
     InsumosModule,
     EquivalenciasMedidasModule,
-    ComandaModule,
-    ComandaProductosModule,
-    ComandaServiciosModule,
-    ComedorModule,
-    DetalleRecetaModule,
-    DiasHabilesModule,
-    EstadosModule,
-    FuncionalidadesModule,
-    ReservacionesModule,
-    RolDeFuncionalidadesModule,
-    SucursalesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
