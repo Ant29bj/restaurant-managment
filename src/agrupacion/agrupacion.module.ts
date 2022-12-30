e554967dd1f709f2a9023abc73a859295c823566
Ant29bj <bojorquez29an@gmail.com>
2022-12-30 11:56:13 -0700

    agrupacion terminada

--- src/agrupacion/agrupacion.module.ts
+++ src/agrupacion/agrupacion.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AgrupacionController } from './agrupacion.controller';
+import { Agrupacion } from './agrupacion.entity';
+import { AgrupacionService } from './agrupacion.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Agrupacion])],
+  controllers: [AgrupacionController],
+  providers: [AgrupacionService],
 })
 export class AgrupacionModule {}
