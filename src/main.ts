5605400e1ea8237523864cdd97d7aef609c1b482
SaulRecio06 <76199088+SaulRecio06@users.noreply.github.com>
2023-04-23 22:00:57 -0700

    Ya quedo la base

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from "@nestjs/core";
+import 'dotenv/config';
 import { AppModule } from "./app.module";
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
